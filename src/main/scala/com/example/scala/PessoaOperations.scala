0b832291f6245592f70d26ab475f8d5910df7221
Giovanni Silva <giovanni@pucminas.br>
2015-03-09 10:18:43 -0300

    Type inference

--- src/main/scala/com/example/scala/PessoaOperations.scala
+++ src/main/scala/com/example/scala/PessoaOperations.scala
@@ -9,16 +9,16 @@ import scala.collection.mutable
  *         09/03/15.
  */
 object PessoaOperations {
-  val nomes: List[String] = List("Mestre", "Dengoso", "Atchim", "Feliz", "Dunga", "Soneca", "Zangado")
-  val sobrenomes: List[String] = List("Schwarzenegger", "Van Dame", "Lee", "Chan", "Stallone", "Lundgren", "Li")
+  val nomes = List("Mestre", "Dengoso", "Atchim", "Feliz", "Dunga", "Soneca", "Zangado")
+  val sobrenomes = List("Schwarzenegger", "Van Dame", "Lee", "Chan", "Stallone", "Lundgren", "Li")
 
   val random = new Random()
 
   def gerarPessoaRandon(idadeMaxima: Int): Pessoa = {
-    val nome: String = nomes(random.nextInt(nomes.size))
-    val sobrenome: String = sobrenomes(random.nextInt(sobrenomes.size))
-    val idade: Int = random.nextInt(idadeMaxima)
-    return new Pessoa(nome, sobrenome, idade)
+    val nome = nomes(random.nextInt(nomes.size))
+    val sobrenome = sobrenomes(random.nextInt(sobrenomes.size))
+    val idade = random.nextInt(idadeMaxima)
+    Pessoa(nome, sobrenome, idade)
   }
 
   def gerarLista(tamanho: Int): mutable.ListBuffer[Pessoa] = {
@@ -26,7 +26,7 @@ object PessoaOperations {
     for (i <- 0 until tamanho) {
       pessoas.append(gerarPessoaRandon(50))
     }
-    return pessoas
+    pessoas
   }
 
   def imprimirLista[T](lista: mutable.ListBuffer[T]) {
